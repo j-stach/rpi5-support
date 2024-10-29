e86e30cc30dbd12a87377c9a278fb26633dc076a
jordan <jordan.stachura@gmail.com>
2024-10-29 11:00:36 -0400

    corrected syntax

--- flake.nix
+++ flake.nix
@@ -1,14 +1,18 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
-  outputs = { nixpkgs, ... }: {
+  # Flake returns Nix kernel for Pi 5
+  outputs = { self, nixpkgs, ... }: {
     legacyPackages.aarch64-linux = with nixpkgs.legacyPackages.aarch64-linux; rec {
 
-      linux_rpi5 = { lib, buildPackages, fetchFromGitHub, perl, buildLinux, rpiVersion, ... } @ args:
-        lib.overrideDerivation( buildLinux( args // {
+      # Custom kernel parameters for NixOS compatability on the Raspberry Pi 5
+      linux_rpi5 =
+        # Base kernel derivation for Raspberry Pi
+        let baseLinux = buildLinux {
           version = "6.1.63-stable_20231123";
           modDirVersion = "6.1.63";
 
+          # Raspberry Pi kernel source
           src = fetchFromGitHub {
             owner = "raspberrypi";
             repo = "linux";
@@ -16,24 +20,36 @@
             hash = "sha256-4Rc57y70LmRFwDnOD4rHoHGmfxD9zYEAwYm9Wvyb3no=";
           };
 
-          defconfig = "bcm2712_defconfig";  # Raspberry Pi 5 only
+          # Kernel config specific to Pi 5
+          defconfig = "bcm2712_defconfig";
 
+          # Disable UEFI boot stub so we can add our own
           features = {
             efiBootStub = false;
-          } // (args.features or {});
+          };
 
+          # Compatibility for Nix and ARM 64-bit architectures
           extraMeta = {
             platforms = with lib.platforms; arm ++ aarch64;
             hydraPlatforms = [ "aarch64-linux" ];
           };
-      } // (args.argsOverride or {}))) (oldAttrs: {
+
+          # Pi-specific hardware support
+          kernelPatches = with kernelPackages; [
+            bridge_stp_helper,
+            request_key_helper
+          ];
+        };
+
+        # Override kernel defaults to ensure Raspberry Pi 5 compatibility
+        in lib.overrideDerivation(baseLinux) (oldAttrs: {
           # Clear the local version in the kernel config to avoid conflict
           postConfigure = ''
             sed -i $buildRoot/.config -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
             sed -i $buildRoot/include/config/auto.conf -e 's/^CONFIG_LOCALVERSION=.*/CONFIG_LOCALVERSION=""/'
           '';
 
-          # Adjust the Device Tree Binary (DTB) filenames for Raspberry Pi 5
+          # Adjust the Device Tree Binary (DTB) for kernel patches
           postFixup = ''
             dtbDir="$out/dtbs/broadcom"
             rm $dtbDir/bcm283*.dtb
@@ -44,8 +60,15 @@
           '';
         });
 
+      # Package the kernel so it can be used in Nix configurations
       linuxPackages_rpi5 = linuxPackagesFor linux_rpi5;
-
     };
   };
 }
+
+
+
+
+
+
+
